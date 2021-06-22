7f20e1e7f13f0e5c2ba418986697e841d1f2bba6
Harrison (Hieu) Van <Hieu_Van@mckinsey.com>
2021-06-22 22:45:23 +0700

    kubectl port-forward

--- scripts/install-hydra.sh
+++ scripts/install-hydra.sh
@@ -13,3 +13,7 @@ helm upgrade --install \
   --namespace=ory --create-namespace \
   --values=../config/hydra.yaml \
   ory-hydra ./
+
+set +x
+printf "\nkubectl port-forward --namespace=ory svc/ory-hydra-public 4444:4444"
+printf "\nkubectl port-forward --namespace=ory svc/ory-hydra-admin 4445:4445\n\n"
--- scripts/install-postgresql.sh
+++ scripts/install-postgresql.sh
@@ -13,3 +13,6 @@ helm upgrade --install \
   --namespace=ory --create-namespace \
   --values=../config/postgresql.yaml \
   ory-postgresql ./
+
+set +x
+printf "\nkubectl port-forward --namespace=ory svc/ory-postgresql 5432:5432\n\n"
