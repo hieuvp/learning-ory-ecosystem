fdb531632303f71cb1767884ff3fd870dfafbead
Harrison (Hieu) Van <Hieu_Van@mckinsey.com>
2021-06-27 15:44:20 +0700

    quickstart-hydra

--- scripts/quickstart-hydra.sh
+++ scripts/quickstart-hydra.sh
@@ -7,4 +7,14 @@ cd quickstart/hydra
 docker-compose \
   --file=quickstart.yml \
   --file=quickstart-postgres.yml up \
-  --build --force-recreate
+  --build --force-recreate --detach
+
+docker-compose \
+  --file=quickstart.yml \
+  exec hydra \
+  hydra token user \
+  --client-id auth-code-client \
+  --client-secret auth-code-secret \
+  --endpoint http://127.0.0.1:4444/ \
+  --port 5555 \
+  --scope openid,offline
